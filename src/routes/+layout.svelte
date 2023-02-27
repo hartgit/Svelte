d2377642ba97f04aa42aff12bbcc20baf64a622e
Lorna Hart (1000114994) <HARTLR3@student.op.ac.nz>
2023-02-28 09:54:57 +1300

    made layout and nav pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    import Navigation from '$lib/+nav.svelte';
+</script>
+
+
+
+<Navigation/>
+
+<br/>
+<slot/>
+<br/>
+
+<footer>
+    <Navigation/>
+</footer>
+and i am some common footer stuff
