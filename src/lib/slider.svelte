4c2f7c6839a6d72eb887e6ba344ee6e7ab1149dc
Lorna Hart (1000114994) <HARTLR3@student.op.ac.nz>
2023-03-31 11:28:15 +1300

    Add text to cards, transitions, refactoring

--- src/lib/slider.svelte
+++ src/lib/slider.svelte
@@ -1,15 +1,23 @@
 <section class="slider-container">
   <section class="cards">
-    <img src="images/grass.jpg" />
-    <img src="images/sunset.jpg" />
-    <img src="images/deer.jpg" />
+    <div class="img-container">
+      <img src="images/grass.jpg" />
+      <div class="text-overlay">hello</div>
+    </div>
+    <div class="img-container">
+      <img src="images/sunset.jpg" />
+      <div class="text-overlay">hello</div>
+    </div>
+    <div class="img-container">
+      <img src="images/deer.jpg" />
+      <div class="text-overlay">hello</div>
+    </div>
   </section>
 </section>
 
 <style>
-  .slider-container {
+  /* .slider-container {
     display: flex;
-
     justify-content: center;
     align-items: center;
     align-content: space-between;
@@ -31,5 +39,74 @@
     filter: grayscale();
     opacity: 0.5;
     box-shadow: 0 0 50px rgb(103, 107, 107);
+  }
+
+  .container {
+    position: relative;
+    text-align: center;
+    color: white;
+  }
+  .container img {
+    flex-wrap: wrap;
+    object-fit: cover;
+    border-radius: 30px;
+    margin: 50px;
+    width: 250px;
+    height: 350px;
+    z-index: -2;
+  }
+
+  .centered {
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  } */
+  .slider-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: space-between;
+  }
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .cards > .img-container {
+    position: relative;
+  }
+  .cards > .img-container > img {
+    background-color: rgb(226, 238, 236);
+    border-radius: 30px;
+    flex-wrap: wrap;
+    width: 250px;
+    height: 350px;
+    margin: 50px;
+    opacity: 1;
+    z-index: 1;
+  }
+  .cards > .img-container > .text-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    color: white;
+    z-index: 2;
+  }
+  .img-container:hover > img {
+    transform: scale(1.2);
+    filter: grayscale();
+    opacity: 0.5;
+    box-shadow: 0 0 50px rgb(103, 107, 107);
+    transition: transform 0.5s;
   }
 </style>
