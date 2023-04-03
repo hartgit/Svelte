411bb05309bb1e4307089b47cc8d5b9ad502016a
Lorna Hart (1000114994) <HARTLR3@student.op.ac.nz>
2023-04-04 08:39:59 +1200

    Fix text in cards

--- src/lib/slider.svelte
+++ src/lib/slider.svelte
@@ -1,22 +1,74 @@
 <section class="slider-container">
   <section class="cards">
-    <div class="img-container">
-      <img src="images/grass.jpg" />
+    <div
+      class="img-container"
+      style="background-image:url(images/grass.jpg); background-size: cover"
+    >
+      <div class="text-overlay">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam
+        leo id maximus facilisis.
+      </div>
+    </div>
+    <div
+      class="img-container"
+      style="background-image:url(images/sunset.jpg); background-size: cover"
+    >
       <div class="text-overlay">hello</div>
     </div>
-    <div class="img-container">
-      <img src="images/sunset.jpg" />
-      <div class="text-overlay">hello</div>
-    </div>
-    <div class="img-container">
-      <img src="images/deer.jpg" />
+    <div
+      class="img-container"
+      style="background-image:url(images/deer.jpg); background-size: cover"
+    >
       <div class="text-overlay">hello</div>
     </div>
   </section>
 </section>
 
 <style>
-  /* .slider-container {
+  .slider-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: space-between;
+  }
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .cards > .img-container {
+    display: flex;
+    height: 350px;
+    width: 250px;
+    margin: 50px;
+    border-radius: 30px;
+    justify-content: center;
+    align-items: center;
+    transition: transform 0.5s;
+  }
+
+  .cards > .img-container > .text-overlay {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 25px;
+    font-size: 24px;
+    color: white;
+    z-index: 2;
+  }
+  .img-container:hover {
+    transform: scale(1.2);
+    filter: grayscale();
+    opacity: 0.5;
+    box-shadow: 0 0 50px rgb(103, 107, 107);
+  }
+
+  /* old cards
+    
+    .slider-container {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -64,49 +116,4 @@
     transform: translate(-50%, -50%);
     z-index: 1;
   } */
-  .slider-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: space-between;
-  }
-  .cards {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-  }
-  .cards > .img-container {
-    position: relative;
-  }
-  .cards > .img-container > img {
-    background-color: rgb(226, 238, 236);
-    border-radius: 30px;
-    flex-wrap: wrap;
-    width: 250px;
-    height: 350px;
-    margin: 50px;
-    opacity: 1;
-    z-index: 1;
-  }
-  .cards > .img-container > .text-overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 24px;
-    color: white;
-    z-index: 2;
-  }
-  .img-container:hover > img {
-    transform: scale(1.2);
-    filter: grayscale();
-    opacity: 0.5;
-    box-shadow: 0 0 50px rgb(103, 107, 107);
-    transition: transform 0.5s;
-  }
 </style>
