7bad3978ed79b7d4b2ce95ee8ff833e287289117
Lorna Hart (1000114994) <HARTLR3@student.op.ac.nz>
2023-06-20 10:27:43 +1200

    typo fix

--- src/lib/cards.svelte
+++ src/lib/cards.svelte
@@ -29,7 +29,7 @@
         weatherData[idx].icon = data.weather[0].icon;
 
         let weatherGroup = parseInt(weatherData[idx].weather / 100);
-        console.log(weatherData[idx]); 
+        console.log(weatherData[idx].icon); 
 
         //if statement sets query to current weather condition
         let query;
